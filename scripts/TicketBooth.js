665c80e4fdb104444587ee9547e0a0a3b72253fb
Charles Butterworth <charlessbutterworth@gmail.com>
2020-11-09 16:11:25 -0600

    got the ride button working

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,22 +1,58 @@
-const contentTarget = document.querySelector(".entry")
-const eventHub = document.querySelector("#state-fair")
 
-// contentTarget.addEventListener("#state-fair", clickEvent => {
+
+
+
+// const contentTarget = document.querySelector(".entry")
+// const eventHub = document.querySelector("#state-fair")
+
+// eventHub.addEventListener("click", clickEvent => {
+// if(clickEvent.target.id === "attraction") {
+
+//     // const selectedRide = clickEvent.target.value 
+
+//     const rideEvent = new CustomEvent("rideTicketPurchased", {
+
+//     detail: {
+//         click: clickEvent.target.id
+//     }
+    
+    
+// })
+// eventHub.dispatchEvent(rideEvent)
 
 // }
+// // console.log("custom event", eventHub)
+// })
 
 
+// export const TicketBooth = () => {
+//     contentTarget.innerHTML = `
+//         <div class="ticketBooth">
+//         <button id="rideTicket">Ride Ticket</button>
+//         </div>
+//     `
+// }
+
+const contentTarget = document.querySelector(".entry")
+const eventHub = document.querySelector("#state-fair")
+eventHub.addEventListener("click", (clickEvent) => {
+    if (clickEvent.target.id === "rideTicket") {
+        const rideEvent = new CustomEvent("rideTicketPurchased", {
+            detail: {
+                clickButton: clickEvent.target.id
+            }
+        });
+        eventHub.dispatchEvent(rideEvent)
+        // console.log("button was clicked")
+    }
+})
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
-        <div class="ticketBooth">
-        <button id="rideTicket">Ride Ticket</button>
-        <button id="foodTicket">Food Ticket</button>
-        <button id="gameTicket">Game Ticket</button>
-        <button id="sideShowTicket">Sideshow Ticket</button>
-        <button id="fullPackageTicket">Full Package Ticket</button>
+        <div class="rideBooth">
+            <button id="rideTicket">Ride Ticket</button>
         </div>
     `
-}
+ }
 
 
 
